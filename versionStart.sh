ee6f88dfe95377826fb43d4ef6ee52f4c35b5be8
Programmer Without a Name <Talk@programmerWithoutA.Name>
2014-08-22 13:57:53 -0400

    fixed shell script

--- versionStart.sh
+++ versionStart.sh
@@ -8,26 +8,32 @@ containername="todomvc"
 containerid=`cat $cidFileName`
 
 image=`docker images | grep todomvc | grep -w $version`
-container=`docker ps -a | grep containerid`
+container=`docker ps -a | grep $containerid`
 
+echo "removing old container"
+docker stop $containerid
+docker rm $containerid
 
-if [$container != ""];
-then
-    docker stop CONTAINER $container
-    docker rm CONTAINER $container
-fi
 
 if [$image != ""];
 then
+    echo "removing old image"
     docker rmi $imagename
 fi
 
+echo "removing old cidFile"
 rm -f -r $cidFileName
 
+echo "building image"
 docker build --rm -t $imagename .
 
 #echo todomvc=$(docker run -i -t -v "$PWD:/data" -p 3000:3000 --name "$container" $imagename)
-docker run -i -t -v "$PWD:/data" -p 3000:3000 --name "$container" --cidfile="$cidFileName" $imagename
+echo "running container"
+docker run -i -t -v "$PWD:/data" -p 3000:3000 --cidfile="$cidFileName" $imagename
+
+# clear out nones.
+sudo docker rmi $( sudo docker images | grep '<none>' | tr -s ' ' | cut -d ' ' -f 3)
+
 #echo "todomvc_cid=$todomvc" > latest
 
 
