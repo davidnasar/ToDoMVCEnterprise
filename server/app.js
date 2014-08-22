2d07fd697c512d16a3de049e11a8a4a264e4cec6
Corinna Cohn <ccohn@fusionalliance.com>
2014-08-22 12:31:58 -0400

    Pointed the server app to ./client

--- server/app.js
+++ server/app.js
@@ -19,9 +19,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'client')));
 
-app.use('/', routes);
+//app.use('/', routes);
 app.use('/users', users);
 
 /// catch 404 and forward to error handler
