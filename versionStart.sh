2ecbbc9e69e5a0106cb5dca6d04103e3e475f616
Programmer Without a Name <Talk@programmerWithoutA.Name>
2014-08-22 13:27:58 -0400

    added shell script

--- versionStart.sh
+++ versionStart.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 clear
 
+cidFileName="todomvc_cid"
 version=`grep version package.json | grep -oe [0-9\.]*`
 imagename=`echo "todomvc:$version"`
 containername="todomvc"
+containerid=`cat $cidFileName`
 
 image=`docker images | grep todomvc | grep -w $version`
-container=`docker ps -a | grep $imagename`
+container=`docker ps -a | grep containerid`
 
-if -n $container
+
+if [$container != ""];
 then
-    docker stop $container
-    docker rm $container
+    docker stop CONTAINER $container
+    docker rm CONTAINER $container
 fi
 
-if -n $image
+if [$image != ""];
 then
     docker rmi $imagename
 fi
 
-
+rm -f -r $cidFileName
 
 docker build --rm -t $imagename .
 
 #echo todomvc=$(docker run -i -t -v "$PWD:/data" -p 3000:3000 --name "$container" $imagename)
-docker run -i -t -v "$PWD:/data" -p 3000:3000 --name "$container" $imagename
+docker run -i -t -v "$PWD:/data" -p 3000:3000 --name "$container" --cidfile="$cidFileName" $imagename
 #echo "todomvc_cid=$todomvc" > latest
 
 
